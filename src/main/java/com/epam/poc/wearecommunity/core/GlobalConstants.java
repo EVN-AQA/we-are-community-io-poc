f57408977d034c26cd47a47ebee6f42ba437454f
Duyen Nguyen <thuduyen07@gmail.com>
2023-01-30 14:27:48 +0700

    AB#417-update code for Hook class (#2)

--- src/main/java/com/epam/poc/wearecommunity/core/GlobalConstants.java
+++ src/main/java/com/epam/poc/wearecommunity/core/GlobalConstants.java
@@ -7,4 +7,7 @@ public class GlobalConstants {
     public static final String SHORT_TIMEOUT_KEY = "short.timeout";
     public static final String LONG_TIMEOUT_KEY = "long.timeout";
 
+    public static final String PAGE_URL = "page.url";
+    public static final String BROWSER_NAME = "browser";
+
 }
--- src/main/java/com/epam/poc/wearecommunity/utilities/Hook.java
+++ src/main/java/com/epam/poc/wearecommunity/utilities/Hook.java
@@ -1,4 +1,38 @@
 package com.epam.poc.wearecommunity.utilities;
 
+import com.epam.poc.wearecommunity.configs.drivers.DriverConfig;
+import com.epam.poc.wearecommunity.core.GlobalConstants;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
 public class Hook {
+
+    protected static WebDriver driver = null;
+    protected Logger logger;
+
+    public Hook(){
+        logger = LogManager.getLogger(getClass());
+    }
+
+    @Before
+    public void setUp() {
+        String pageUrl = new PropertyReader(GlobalConstants.PAGE_URL).getValue(GlobalConstants.PAGE_URL);
+        String browserName = new PropertyReader(GlobalConstants.BROWSER_NAME).getValue(GlobalConstants.BROWSER_NAME).toUpperCase();
+
+        logger.info("Page Url: " + pageUrl + "with browser name: " + browserName);
+        driver = DriverConfig.getDriver(browserName);
+        driver.get(pageUrl);
+    }
+
+    @After
+    public void tearDown() {
+        DriverConfig.quitDriver();
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
 }
